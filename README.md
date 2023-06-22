d884d545dc4fa13a0253605a9c6b23bbdde82215
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-22 20:23:29 +0300

    APIs work

---
+++ README.md
@@ -0,0 +1,3 @@
+Tutorial link is https://www.youtube.com/watch?v=G3lPb6mlqTA&ab_channel=BIInsightsInc
+
+Test it over postman or Insomnia
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from distutils.debug import DEBUG
+from distutils.log import debug
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
@@ -14,7 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #why do this? --flask serve
 
 #SQLALchemy mapper --Help interact with the db
 db = SQLAlchemy(app)
-app.app_context().push() #solves working out of application context
+# app.app_context().push() #solves working out of application context
 
 #define a class Employee  --store and retrieve data from Employee table in db
 class Employee(db.Model):
@@ -45,3 +47,69 @@ class Employee(db.Model):
 # END OF CREATING DB
 
 #Creating API Routes Endpoints
+
+
+
+#--For GET request to http://localhost:5000/
+class GetEmployee(Resource):
+    def get(self):
+        employees = Employee.query.all()
+        emp_list = []
+        for emp in employees:
+            emp_data = {'Id':emp.id, 'FirstName':emp.firstname, 'LastName':emp.lastname, 'Gender':emp.gender,
+                        'Salary':emp.salary}
+            emp_list.append(emp_data)
+        return {"Employees":emp_list}, 200
+
+
+#--For Post request to http://localhost:5000/employee
+class AddEmployee(Resource):
+    def post(self):
+        if request.is_json:
+            emp = Employee(firstname=request.json['FirstName'], lastname=request.json['LastName'],
+                    gender=request.json['Gender'], salary=request.json['Salary'])
+            db.session.add(emp)
+            db.session.commit()
+
+            #return a json response
+            return make_response(jsonify({'Id':emp.id, 'First Name': emp.firstname, 'Last Name': emp.lastname,
+                                    'Gender':emp.gender, 'Salary':emp.salary}), 201)
+        else:
+            return {'error':'Request must be JSON'}, 400
+
+#--For Put request to http://localhost:5000/update/? --takes user id to update
+class UpdateEmployee(Resource):
+    def put(self, id):
+        if request.is_json:
+            emp = Employee.query.get(id)
+            if emp is None:
+                return {'error':'not found'}, 404
+            else:
+                emp.firstname = request.json['FirstName']
+                emp.lastname = request.json['LastName']
+                emp.gender = request.json['Gender']
+                emp.salary = request.json['Salary']
+                db.session.commit()
+                return 'Updated', 200
+        else:
+            return {'error':'Request must be JSON'}, 400
+
+#--For Delete request to http://localhost:5000/delete? -- takes an id of user to delete
+class DeleteEmployee(Resource):
+    def delete(self, id):
+        emp = Employee.query.get(id)
+        if emp is None:
+            return {'error':'not found'}, 404
+        
+        db.session.delete(emp)
+        db.session.commit()
+        return f'{id} is deleted', 200 
+
+#use add_Resource function to register a route to one of the classes above
+api.add_resource(GetEmployee, '/')
+api.add_resource(AddEmployee, '/add')
+api.add_resource(UpdateEmployee, '/update/<int:id>')
+api.add_resource(DeleteEmployee, '/delete/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
